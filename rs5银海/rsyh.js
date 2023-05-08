b0166233f4ef925124a6031922c876dd5c1bab1a
hott1ng <102499662+hott1ng@users.noreply.github.com>
2023-05-08 23:24:43 +0800

    Add files via upload

---
+++ rs5银海/rsyh.js
@@ -0,0 +1,110 @@
+delete global.__filename;
+delete global.__dirname;
+
+window = global;
+window.top = window
+window.setInterval = function(){console.log(arguments)}
+
+
+window.attachEvent = function (){}
+window.addEventListener = function (){}
+
+
+Object.prototype.getAttribute = function(val){
+    if(val==='r'){
+        return 'm'
+    }
+}
+
+
+
+div = {getElementsByTagName:function(val){
+        if(val==='i'){
+                return {length:0}
+        }
+        }}
+document = {
+appendChild : function (){console.log(arguments)},
+removeChild  :function (){console.log(arguments)},
+     attachEvent: function () {
+    },
+    addEventListener: function () {
+    },
+    documentElement:{},
+    characterSet: 'UTF-8',
+        charset: 'UTF-8',
+        script:['script','script'],
+        createElement: function (val) {
+        if (val === 'div') {
+            return div
+        }
+        if(val ==='form'){
+            return {}
+        }
+        if(val==='a'){
+            return{}
+        }
+        },
+       getElementsByTagName: function () {
+     return [{
+            content: content, parentNode: {
+                removeChild: function () {
+                }
+            }, parentElement: {
+                removeChild: function () {
+                }
+            }
+        },
+            {
+                content: content, parentNode: {
+                    removeChild: function () {
+                    }
+                }, parentElement: {
+                    removeChild: function () {
+                    }
+                }
+            }
+        ]
+},
+      getElementById:function(){return [{},{}]}
+
+}
+
+location={
+    "ancestorOrigins": {},
+    "href": "http://www.chinastock.com.cn/newsite/cgs-services/stockFinance/businessAnnc.html",
+    "origin": "http://www.chinastock.com.cn",
+    "protocol": "http:",
+    "host": "www.chinastock.com.cn",
+    "hostname": "www.chinastock.com.cn",
+    "port": "",
+    "pathname": "/newsite/cgs-services/stockFinance/businessAnnc.html",
+    "search": "",
+    "hash": ""
+}
+
+navigator = {
+
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
+}
+
+
+
+
+content = "XaVog5v.i55ehw0UwHLMD77fxgu1ruQod_q5IDK60s44VD33N467tsN5LbWGYWRHXveNKurveOMfKUVwOF7jcWbl.IkKFwmC"
+
+$_ts = window['$_ts'];
+if (!$_ts)
+    $_ts = {};
+$_ts.nsd = 991;
+$_ts.cd = "qoGdrrAlxA7qtrGEcaqLDq9hqcG3cG7qxG3YragKcaqbr13ckf3bqc3rqnWErrVhqc3qxGlimP3lkflmxGWiEGAbqcWxrrGbrAgpqG7qxGWbqqgKcaqbrP3oHp3brcWtrrGhqc3qxGlimfVlkPEmcaqbqP3kkPlbqcW2bAlCWqEmWsKRE4vX2CP3EzLzoyRsFz_3RB.IxyzumWDRQIYGr2OIM4Q8qqVuraQrbcarKT2ZxssKRkljJCrHQQg4WYrNIYr3_uQuIlV2HY.6wbSLhb2nMIVBMvmXFnzB4De7tDSnFCCTMUYLM2L.EeSNKsmpMVET_OwDWUzmMlhU10w0W0q2iZxzwKfXtC20eKZ7Fvw.Fn66wbSLhb2nMIVBMvmXFTLzX0YjFVRuQoIAAKwsiTYOFiqn1krKV93OLTfXQKy.tCd7MCaLMox.MMYzwKfXtC20eKZ7Fvw.FTnTEYY6M0e3KJT2QYYWIVJ9LlenKuYzUCc4iYzjwCz.hIwCMDGXFUxze1eXQKy.tCd7MCaLMox.MQABEbauUvRVjYNfQ2Qu1ls6MCyGAKpKYHGfi2e7QbzzzKTTFDg.FUsTMnSjwCz.hIwCMDGXFUxzeYg7xvwpFmU1VC0T12yWV72sJogSRowgz6EPH22NQb6ThCySMb7.M5SBMcf7QbzzzKTTFDg.FUsTMTTyhb2nMIY8cGs3WCfTd92aJuQZFD4TYo27Y0aZAByCMlS2WVW_.bevWCSlMs8PMbSeiCmpKeQgWVmgY0rCnTRBYKJAHDOrFmN.Mmp.RzNfVse6QmxlgKyHWUwqsUXP1900FmR7wwATp2wstslC.OxfWu3NMUtPMk2mYbE0iIrn3byjtslC.OxfWu3NMUtPMmwnYYThYFmlM6RNHlTSnkwS1kJcJUMqVuznAYTuFBRNKKJswol_umYbYspOFC.WJmr5hklaJjfPJuq7MUA_n9YJVkzHplMaK9ephklaJjfPJuq7MUArbAWrJqEDJGDhF6paQO0CWjg0WuA4JsZCLu7arqASWkFL3OQlqs3SWy9xWsA_Jk3CLkW0JqEkWuDGJAqVtPTxKTtjIWElccttzILWhaEUd8_fx0BsdUaDbxLkrq3mqqQfMufdQ1glbFWj1I35DKg4p4OJ9jm8Uf1g91NMbe96ldCRcOEnWuVuJFaTJaAkJGQhjkVgJu30Wki.WAqBJbNnVJ3TQUyqM0YRuTlTJ9wdWCuzJTwo1oxVRzJLRmSvYuyYdCS7WKY7AGDRqalaqG36qRG0qIZTI1zG5Dq7RKeTtCkjMPS2wDg.3dfLhDROQnzOeoW7RbzNtCtT3Dmbhbp0Q.YGQ6EXRDf7zKrawcyORvKTRCSLhbJL3.YvM6qX3CfPg1e2IDQ.3b4zhCRL3nzfMILBRbz7tCmX41ebFCSutCMe31SOFCl.RXWBRCSztCez_1eLwoE.M6U_hC2vM1zXMdqBFKN9tCyB_PeNwC0.MUtTFKmChbT6R.YNQDm2tCTneb07MbJSRP6TFbELMbfGhIpPQcfBMbWzeoW0tDe6RP60JClLM6Q0hIpuRnfBMolzeDS9tDeLQ160Fb0LM6w6hIpaF1fBwCxbzKSPMPyLRobTMDwOhbf.34Y7MDlXFoJvzUJytox5Rc6N3vWLQbrjh82uM1fnw6Az_UJ0toxO316NQ6pZhvwNR.YuFKQXwCzLzUJu3cyCMb8TQvpZhvYbh8zXhoROFPzueoYPtorP3c6.RvWLQoJfh8zPQPfSFczn5CZ7wCpdt6hX3PS6wKl.QXe9how4wczT5ol7QDJet6UaR1SaFCmfh8T7QvRvFUWz4Uz0toRu316BQDALwop0h8eawnf0ICWz4bSb3PyaFDIdW1SuM6l.wBpPhoJOQ1zC_Uq7IDrjt6vNMPSZMvA.8IYa8cf_wD7zgKy7toG.IKONhCm0F1z6MIrawcf.wC3rbAWlKn3O3aDwUcpGAqQkU.TfVqADKcJP.qWrWAEDHGD3WG";
+if ($_ts.lcd)
+    $_ts.lcd();
+
+
+require('./6771a')
+
+
+console.log(document.cookie)
+
+
